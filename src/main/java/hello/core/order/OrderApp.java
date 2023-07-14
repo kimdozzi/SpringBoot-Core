0d9ecb4bc691f11e96aaaa687a5a6dc1e12bfcfd
kimdozzi <neo5188@gmail.com>
2023-07-14 12:08:23 +0900

    새로운 êµ"새로운 구조와 할ã할인 정책 적용"

--- src/main/java/hello/core/order/OrderApp.java
+++ src/main/java/hello/core/order/OrderApp.java
@@ -1,5 +1,6 @@
 package hello.core.order;
 
+import hello.core.AppConfig;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
@@ -7,14 +8,16 @@ import hello.core.member.MemberServiceImpl;
 
 public class OrderApp {
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl(null);
-        OrderService orderService = new OrderServiceImpl(null, null);
+
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+        OrderService orderService = appConfig.orderService();
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
         memberService.join(member);
 
-        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Order order = orderService.createOrder(memberId, "itemA", 15500);
         System.out.println("order = " + order.toString());
         System.out.println("order calculate = " + order.calculatePrice());
     }
