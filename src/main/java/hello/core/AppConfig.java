4281b20fb00116556205f33cbf8db3b87da707ff
kimdozzi <neo5188@gmail.com>
2023-07-27 23:23:19 +0900

    ComponentScan 알아보기

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -34,7 +34,7 @@ public class AppConfig {
     }
     @Bean
     public DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
-//        return new RateDiscountPolicy();
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
