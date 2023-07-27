d202cf59b750b8f5e5cb000dcef9db14365e54a1
kimdozzi <neo5188@gmail.com>
2023-07-28 00:39:17 +0900

    ComponentScan 의 filter 사 사ì 사용해보기

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,14 +1,16 @@
 package hello.core;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
-//@Configuration
+// @Configuration
 @ComponentScan(
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION,
         classes = Configuration.class)
 )
+@Configuration
 public class AutoAppConfig {
 
 }
