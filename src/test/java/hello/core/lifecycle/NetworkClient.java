6a9c104c4681ebc98a85d15122af8e7d5a3b144f
kimdozzi <neo5188@gmail.com>
2023-08-09 09:41:45 +0900

    @Bean(initMethod = "init", destroyMethod = "close") 사용하기

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,9 +1,6 @@
 package hello.core.lifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
-
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
 
     private String url;
     public void setUrl(String url) {
@@ -32,17 +29,15 @@ public class NetworkClient implements InitializingBean, DisposableBean {
     }
 
 
-    @Override
-    // 의존 관계 주입이 끝나면 호출해주겠다.
-    public void afterPropertiesSet() throws Exception {
-        System.out.println("NetworkClient.afterPropertiesSet");
+
+    public void init() throws Exception {
+        System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
 
-    @Override
-    public void destroy() throws Exception {
-        System.out.println("NetworkClient.destroy");
+    public void close() throws Exception {
+        System.out.println("NetworkClient.close");
         disconnect();
     }
 }
