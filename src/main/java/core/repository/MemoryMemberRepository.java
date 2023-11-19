ad889a909aaa96d4c38ddc261176a3f33d9bd61d
kimdozzi <neo5188@gmail.com>
2023-11-19 20:22:21 +0900

    컴포넌í트 스캔과 자동 의존관계 설정

--- src/main/java/core/repository/MemoryMemberRepository.java
+++ src/main/java/core/repository/MemoryMemberRepository.java
@@ -1,9 +1,11 @@
 package core.repository;
 
 import core.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
+@Repository
 public class MemoryMemberRepository implements MemberRepository {
 
     private static Map<Long, Member> store = new HashMap<>();
--- src/main/java/core/service/MemberService.java
+++ src/main/java/core/service/MemberService.java
@@ -3,13 +3,18 @@ package core.service;
 import core.domain.Member;
 import core.repository.MemberRepository;
 import core.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
     private final MemberRepository memberRepository;
 
+
+    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
