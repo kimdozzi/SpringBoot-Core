894d891a042bf64d42341b7e9d0fdc3ed9522f80
kimdozzi <neo5188@gmail.com>
2023-11-19 19:08:20 +0900

    2회독 시작 및 회원 도메인 및 리포지토리 작성, 테스트

--- src/main/java/core/controller/HelloController.java
+++ src/main/java/core/controller/HelloController.java
@@ -1,4 +1,4 @@
-package hello.core.controller;
+package core.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
--- src/main/java/core/repository/MemoryMemberRepository.java
+++ src/main/java/core/repository/MemoryMemberRepository.java
@@ -1,2 +1,40 @@
-package core.repository;public class MemoryMemberRepository {
+package core.repository;
+
+import core.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        // null일 경우를 대비
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
 }
