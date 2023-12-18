39e00afd217f08dedb1d6503601ce7678afbfb2e
kimdozzi <neo5188@gmail.com>
2023-12-19 08:26:11 +0900

    [의존관계 자동  주입] 다양한 의존관계 주입 방법, 옵션 처리

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -12,7 +12,7 @@ public class OrderServiceImpl implements OrderService{
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-    @Autowired
+    @Autowired // 생성자가 딱 1개만 있으면, Autowired 생략 가능
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
